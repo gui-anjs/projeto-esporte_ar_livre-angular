1208c14e3c9aff162d08f48a0709d803851598e3
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-08-24 20:04:45 -0300

    .

--- src/app/inscricao/inscricao.ts
+++ src/app/inscricao/inscricao.ts
@@ -1,39 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inscricao',
+  imports: [CommonModule, FormsModule],
   templateUrl: './inscricao.html',
-  styleUrls: ['./inscricao.component.css']
+  styleUrls: ['./inscricao.css']
 })
 export class InscricaoComponent {
 
-  // Variáveis ligadas ao [(ngModel)]
   atletaId: string = '';
   corridaId: string = '';
   dataInscricao: string = '';
   statusPagamento: string = '';
 
-  // Listas simuladas para preencher as opções dos selects (depois você puxa do banco)
-  listaAtletas = [
-    { id: '1', nome: 'João Pedro' },
-    { id: '2', nome: 'Maria Silva' }
-  ];
+  listaAtletas: any[] = [];
+  listaCorridas: any[] = [];
 
-  listaCorridas = [
-    { id: '1', descricao: '5ª Maratona dos Cara' },
-    { id: '2', descricao: 'Corrida Noturna 10k' }
-  ];
-
-  // Função disparada no (submit) do form
   salvarInscricao() {
-    // Aqui você vai fazer a chamada pro seu backend/serviço
-    console.log('Salvando inscrição:', {
-      atletaId: this.atletaId,
-      corridaId: this.corridaId,
-      dataInscricao: this.dataInscricao,
-      statusPagamento: this.statusPagamento
-    });
-    
-    alert('Inscrição salva com sucesso!');
+    console.log('Inscrição cadastrada!');
+    console.log('Atleta:', this.atletaId);
+    console.log('Corrida:', this.corridaId);
+    console.log('Data:', this.dataInscricao);
+    console.log('Pagamento:', this.statusPagamento);
   }
+
 }
